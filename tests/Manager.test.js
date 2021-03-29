18255e7ee91d917d7a4b3b438b04f85e3ab91430
Luc Burns <burnsluc2@gmail.com>
2021-03-29 18:09:47 -0400

    Formatted code

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -2,31 +2,31 @@ const { describe, it, expect } = require("@jest/globals");
 const Manager = require("../lib/Manager");
 
 describe("Manager", () => {
-    it("When called, creates a new Manager instance", () => {
-        const manager = new Manager();
-        expect(typeof(manager)).toBe("object");
-    })
+  it("When called, creates a new Manager instance", () => {
+    const manager = new Manager();
+    expect(typeof manager).toBe("object");
+  });
 
-    it("Sets an office number from the constructor", () => {
-        const officeNumber = 102;
-        const manager = new Manager("John", 1, "fake@test.com", officeNumber);
+  it("Sets an office number from the constructor", () => {
+    const officeNumber = 102;
+    const manager = new Manager("John", 1, "fake@test.com", officeNumber);
 
-        expect(manager.officeNumber).toBe(officeNumber);
-    })
+    expect(manager.officeNumber).toBe(officeNumber);
+  });
 
-    describe("getRole", () => {
-        it("Returns manager instead of employee", () => {
-            const manager = new Manager();
-            expect(manager.getRole()).toBe("Manager");
-        }) 
-    })
+  describe("getRole", () => {
+    it("Returns manager instead of employee", () => {
+      const manager = new Manager();
+      expect(manager.getRole()).toBe("Manager");
+    });
+  });
 
-    describe("getOfficeNumber", () => {
-        it("Returns the office number", () => {
-            const officeNumber = 102;
-            const manager = new Manager("John", 1, "fake@test.com", officeNumber);
+  describe("getOfficeNumber", () => {
+    it("Returns the office number", () => {
+      const officeNumber = 102;
+      const manager = new Manager("John", 1, "fake@test.com", officeNumber);
 
-            expect(manager.getOfficeNumber()).toBe(officeNumber);
-        })
-    })
-})
+      expect(manager.getOfficeNumber()).toBe(officeNumber);
+    });
+  });
+});
