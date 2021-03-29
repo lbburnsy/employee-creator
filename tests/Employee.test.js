a25222c073b40038886df8da3e0c31ef048762a1
Luc Burns <burnsluc2@gmail.com>
2021-03-29 17:50:14 -0400

    Manager test and class done

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,26 +1,64 @@
 const { expect, it } = require("@jest/globals");
-const { describe } = require("yargs");
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
     it("When called, creates a new Employee instance", () => {
         const employee = new Employee();
         expect(typeof(employee)).toBe("object");
-    });
+    })
+
+    it("Sets a name value from the constructor", () => {
+        const name = "John";
+        const employee = new Employee(name);
+
+        expect(employee.name).toBe(name);
+    })
+
+    it("Sets an id value from the constructor", () => {
+        const id = 1;
+        const employee = new Employee("John", id);
+
+        expect(employee.id).toBe(id);
+    })
+
+    it("Sets an email value from the constructor", () => {
+        const email = "fake@test.com";
+        const employee = new Employee("John", 1, email);
+
+        expect(employee.email).toBe(email);
+    })
 
     describe("getName", () => {
+        it("Returns a name value", () => {
+            const name = "John";
+            const employee = new Employee(name);
 
+            expect(employee.getName()).toBe(name);
+        })
     });
 
     describe("getID", () => {
+        it("Returns an ID", () => {
+            const id = 1;
+            const employee = new Employee("John", id);
 
+            expect(employee.getID()).toBe(id);
+        })
     });
 
     describe("getEmail", () => {
+        it("Returns an email", () => {
+            const email = "fake@test.com";
+            const employee = new Employee("John", 1, email);
 
+            expect(employee.getEmail()).toBe(email);
+        })
     });
 
     describe("getRole", () => {
-
+        it("Returns a role", () => {
+            const employee = new Employee();
+            expect(employee.getRole()).toBe("Employee");
+        })
     })
 })
---
+++ tests/Manager.test.js
@@ -0,0 +1,32 @@
+const { describe, it, expect } = require("@jest/globals");
+const Manager = require("../lib/Manager");
+
+describe("Manager", () => {
+    it("When called, creates a new Manager instance", () => {
+        const manager = new Manager();
+        expect(typeof(manager)).toBe("object");
+    })
+
+    it("Sets an office number from the constructor", () => {
+        const officeNumber = 102;
+        const manager = new Manager("John", 1, "fake@test.com", officeNumber);
+
+        expect(manager.officeNumber).toBe(officeNumber);
+    })
+
+    describe("getRole", () => {
+        it("Returns manager instead of employee", () => {
+            const manager = new Manager();
+            expect(manager.getRole()).toBe("Manager");
+        }) 
+    })
+
+    describe("getOfficeNumber", () => {
+        it("Returns the office number", () => {
+            const officeNumber = 102;
+            const manager = new Manager("John", 1, "fake@test.com", officeNumber);
+
+            expect(manager.getOfficeNumber()).toBe(officeNumber);
+        })
+    })
+})
