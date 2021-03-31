2d84c26dfb1c71cf0dd6bd2c7175f3aeaa149965
Luc Burns <burnsluc2@gmail.com>
2021-03-30 21:40:01 -0400

    App skeleton

---
+++ index.js
@@ -0,0 +1,9 @@
+// Call all required modules
+
+// Do I need an array or something to store?
+
+// A function to handle the inquirer prompts
+
+// Need to figure out how to write and/or append html
+
+// Function to init app?
