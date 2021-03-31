090402068bf31eb03c7525f05461c325b5ac8715
Luc Burns <burnsluc2@gmail.com>
2021-03-30 21:55:57 -0400

    First set of inquirer questions

--- index.js
+++ index.js
@@ -9,6 +9,36 @@ const Manager = require("./lib/Manager");
 const employees = [];
 
 // A function to handle the inquirer prompts
+function addEmployee() {
+    inquirer.prompt([
+        {
+            message: "Enter employee's name:",
+            name: "name"
+        },
+        {
+            message: "Enter employee's ID number:",
+            name: "id"
+        },
+        {
+            message: "Enter employee's email:",
+            name: "email"
+        },
+        {
+            type: "list",
+            message: "What is this employee's role?",
+            choices: [
+                "Intern",
+                "Engineer",
+                "Manager"
+            ],
+            name: "role"
+        }
+    ]).then((data) => {
+        console.log(data);
+    })
+}
+
+addEmployee();
 
 // Need to figure out how to write and/or append html
 
