cccc4cb86383ae3668daabe08a21bc11245fbc2f
Luc Burns <burnsluc2@gmail.com>
2021-03-30 21:40:21 -0400

    Linked all required modules

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Call all required modules
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
 
 // Do I need an array or something to store?
 
